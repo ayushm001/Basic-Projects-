140fb75a17f7aaad4598b38bffab01750a66d49d
Ayushman Rout <161948217+ayushm001@users.noreply.github.com>
2024-10-27 22:52:07 +0530

    Add files via upload

---
+++ Weather Web/script.js
@@ -0,0 +1,5 @@
+const temp = document.getElementsByClassName('.temprature');  // for temrature 
+const city = document.getElementsByClassName('.city')
+
+const humidty = document.getElementsByName('.details1')
+const wind = document.getElementsByName('.details2'
